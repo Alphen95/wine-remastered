40a4a80ae73b0e1dfdff672fc919e0ac7834bcc7
Alphen95 <57586762+Alphen95@users.noreply.github.com>
2021-06-02 21:50:18 +0500

    i'm too lazy to rewrite the whole info
    added readme and cleaned up

--- main.py
+++ main.py
@@ -1,38 +1,44 @@
-import datetime, pandas
+import datetime, pandas,argparse
 from pprint import pprint
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from collections import defaultdict
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+parser = argparse.ArgumentParser(
+    description='Запускает сайт по продаже вина, с использованием .xlsx файла.'
+)
+first_opening_year = 1920
+parser.add_argument('--xlsx_file', help='Пользовательский файл')
+args = parser.parse_args()
+if type(args.xlsx_file) != str:
+  file = 'base_file.html'
+else:
+  print(str(args.xlsx_file))
+  file = args.xlsx_file
 enviroment = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
 )
 
 organized_dict_wine_cards = defaultdict(list)
-template = enviroment.get_template('base_file.html')
+template = enviroment.get_template(file)
 
-excelfile = pandas.read_excel('wine3.xlsx', sheet_name='Лист1', usecols=['Категория','Название', 'Сорт','Цена','Картинка',"Акция"])
+excelfile = pandas.read_excel('wine_placeholder.xlsx', sheet_name='Лист1', usecols=['Категория','Название', 'Сорт','Цена','Картинка',"Акция"])
 wine_cards_dict = excelfile.to_dict(orient='record')
 for bottle in wine_cards_dict:
-  print(bottle["Название"])
+  print
   try:
     bottle["Сорт"] = "Сорт винограда - " + bottle["Сорт"]
   except:
     bottle["Сорт"] = " "
   bottle["Цена"] = str(bottle["Цена"]) + "р."
   bottle["Картинка"] = "images/" + bottle["Картинка"]
-  if bottle["Категория"] == "Белые вина":
-    organized_dict_wine_cards["Белые вина"].append(bottle)
-  elif bottle["Категория"] == "Напитки":
-    organized_dict_wine_cards["Напитки"].append(bottle)
-  else:
-    organized_dict_wine_cards["Красные вина"].append(bottle)
+  bottle_card_type =bottle["Категория"]
+  organized_dict_wine_cards[bottle_card_type].append(bottle)
     
-date_today = str(datetime.date.today())
-year_today = date_today[:4]
-years_with_you = int(year_today) - 1920
+today = datetime.datetime.now()
+years_with_you = today.year - first_opening_year
 
 rendered_page = template.render(
     years_with_our_clients="Уже "+str(years_with_you)+" лет с вами.",
@@ -41,6 +47,6 @@ rendered_page = template.render(
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
-pprint(organized_dict_wine_cards)
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+pprint(organized_dict_wine_cards)
 server.serve_forever()
