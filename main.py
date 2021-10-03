ca0d6a87cf64b5329d5e97b457e706829dab3e94
Alphen95 <57586762+Alphen95@users.noreply.github.com>
2021-10-03 13:55:13 +0500

    update #2
    splitted library loading
    added comment so everyone would understand why there is that line
    renamed argument metavar
    changed defaultdict stuff, now everything is added in html
    added ifmain
    fixed readme.md and added a example table for better understanding what to do

--- main.py
+++ main.py
@@ -1,33 +1,37 @@
-import datetime, pandas,argparse
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-from collections import defaultdict
+import datetime
+import argparse
 import pathlib
 import os
+
+import pandas
+
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from collections import defaultdict
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 parser = argparse.ArgumentParser(
     description='Запускает сайт по продаже вина, с использованием .xlsx файла.'
 )
-path_folder = str(pathlib.Path(__file__).parent.absolute())
-os.chdir(path_folder)
+#эта штука важная, без неё будет криво работать погдгрузка файлов, потому что не все люди будут напрямую из коммандой строки запускать, а через .bat или что-то похожее.
 opening_year = 1920
-parser.add_argument('xlsx_file', metavar="N", type=str, help='Пользовательский файл')
+parser.add_argument('xlsx_file_name', metavar="file", type=str, help='Пользовательский файл')
+os.chdir(str(pathlib.Path(__file__).parent.absolute()))
 args = parser.parse_args()
-xlsx_table_filename = args.xlsx_file
+xlsx_table_filename = args.xlsx_file_name
 enviroment = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
 )
-
 organized_wine_cards = defaultdict(list)
 template = enviroment.get_template("template.html")
 
+
 def main():
-  excelfile = pandas.read_excel(xlsx_table_filename, sheet_name='Лист1', usecols=['Категория','Название', 'Сорт','Цена','Картинка',"Акция"])
-  wine_cards = excelfile.to_dict(orient='record')
+  wine_table = pandas.read_excel(xlsx_table_filename, sheet_name='Лист1', usecols=['Категория','Название', 'Сорт','Цена','Картинка',"Акция"])
+  wine_cards = wine_table.to_dict(orient='record')
   for bottle in wine_cards:
-    organized_wine_cards[bottle["Категория"]].append({"class":"Сорт винограда - {}".format(bottle["Сорт"]) if bottle["Сорт"] !="None" else " ","cost":"{} р.".format(str(bottle["Цена"])),"picture":"images/{}".format(bottle["Картинка"]),"name":bottle["Название"],"stock":bottle["Акция"]})
+    organized_wine_cards[bottle["Категория"]].append(bottle)
       
   today = datetime.datetime.now()
   years_with_you = today.year - opening_year
@@ -42,4 +46,5 @@ def main():
   server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
   server.serve_forever()
 
-main()
+if __name__ == "__main__":
+  main()
